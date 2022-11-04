85509172e0ca5d9e21e2dd9471634ead592f3c48
mru4913 <yumengda@py-star.com>
2022-11-04 15:19:18 +0800

    feat(algo): update DetectNutLoose3d_v1 by adding at_least_r_depth and min_depth_value

--- algo/defectType4.py
+++ algo/defectType4.py
@@ -18,7 +18,7 @@ from pystar360.utilities._logger import d_logger
 @algoDecorator
 class DetectNutLoose3d_v1(algoBaseABC):
     """
-    检测螺帽/螺帽松动（凸出来螺帽状态）
+    检测螺帽/螺帽松动（凸出来螺帽的状态）
     本流程通过2d流程定位，curr_rect映射到curr_rect3d 直接映射到3点图像，所以不需要yolo，其中图片裁剪框用的是curr_rect3d
 
     depth_items:
@@ -29,13 +29,17 @@ class DetectNutLoose3d_v1(algoBaseABC):
                 top_plane_offset: 3 # 螺帽最高平面允许的深度值波动范围
                 contour_expand_pixel: 1 # 螺帽边缘扩张
                 min_nonzero_ratio: 0.4 # 如果非零数在深度图占了40%以下，说明深度值丢失
-                depth_thres: 17 # 螺帽顶和其周边的深度值差。阈值设置
+                relative_depth_thres: 17 # 螺帽顶和其周边的深度值差。阈值设置，不能超过这个相对高度不然报警
+                min_depth_value: 0 # 这个区域不可能出现比这个小的值 default=0
+                at_least_r_depth: None # 螺帽最小需要这个相对高度, 参考用的正常高度。 relative_depth_thres > at_least_r_depth >= top_plane_offset
     """
 
     TOP_PLANE_OFFSET = 3
     CONTOUR_EXPAND_PIXEL = 2
-    DEPTH_THRES = 100
+    RELATIVE_DEPTH_THRES = 100
     MIN_NONZERO_RATIO = 0.4
+    MIN_DEPTH_VALUE = 0
+    AT_LEAST_R_DEPTH = None
 
     def __call__(self, item_bboxes_list, test_img, test_startline, img_h, img_w, **kwargs):
         # if empty, return empty
@@ -44,8 +48,10 @@ class DetectNutLoose3d_v1(algoBaseABC):
 
         top_plane_offset = self.item_params.get("top_plane_offset", self.TOP_PLANE_OFFSET)
         contour_expand_pixel = self.item_params.get("contour_expand_pixel", self.CONTOUR_EXPAND_PIXEL)
-        depth_thres = self.item_params.get("depth_thres", self.DEPTH_THRES)
+        relative_depth_thres = self.item_params.get("relative_depth_thres", self.RELATIVE_DEPTH_THRES)
         min_nonzero_ratio = self.item_params.get("min_nonzero_ratio", self.MIN_NONZERO_RATIO)
+        min_depth_value = self.item_params.get("min_depth_value", self.MIN_DEPTH_VALUE)
+        at_least_r_depth = self.item_params.get("at_least_r_depth", self.AT_LEAST_R_DEPTH)
 
         # iterate
         new_item_bboxes_list = []
@@ -67,10 +73,12 @@ class DetectNutLoose3d_v1(algoBaseABC):
             rect_p = frame2rect(rect_f, test_startline, img_h, img_w, axis=self.axis)
             item_img = crop_segmented_rect(test_img, rect_p)
             item_img_h, item_img_w = item_img.shape
-            # img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+            # image preprocess
+            item_img = cv2.medianBlur(item_img, 3)
 
-            img_nonzero = item_img[item_img != 0]
-            # if the number of 0 is greather than 70% of the image size, it means the loss of depth value. Skip detection"
+            # img_nonzero = item_img[item_img != 0]
+            img_nonzero = item_img[item_img > min_depth_value]
+            # if the number of 0 is greather than xx% of the image size, it means the loss of depth value. Skip detection"
             if img_nonzero.size / item_img.size < min_nonzero_ratio:
                 box.description = f">>> Depth value loss. Skip depth detection"
                 box.is_detected = 1
@@ -79,12 +87,11 @@ class DetectNutLoose3d_v1(algoBaseABC):
                 count += 1
                 break
 
-            # 螺栓的顶部，在定位后，通常都是最高的数值
+            # 确定螺栓的顶部深度值，在定位后，通常都是最高的数值平面
             hist, bin_edges = np.histogram(
                 img_nonzero.flatten(), bins=list(range(img_nonzero.min(), img_nonzero.max() + 1))
             )
             hist, bin_edges = np.append(hist[::-1], [0]), bin_edges[::-1]
-
             find_hat_plane = 0
             for idx, (c, d) in enumerate(zip(hist, bin_edges)):
                 if idx > 4:
@@ -93,54 +100,68 @@ class DetectNutLoose3d_v1(algoBaseABC):
                     find_hat_plane = 1
                     max_val = d
                     break
-
             if not find_hat_plane:
                 max_val = np.max(img_nonzero)
 
             # get hat height
             hat_mask = (
-                (item_img > max_val - top_plane_offset) & (item_img < max_val + top_plane_offset) & (item_img != 0)
+                (item_img > max_val - top_plane_offset)
+                & (item_img < max_val + top_plane_offset)
+                & (item_img > min_depth_value)
             )
             hat_height = np.median(item_img[hat_mask])
-            # print("****", max_val)
-            # print(item_img[hat_mask])
-            # print(f"hat_height {hat_height}")
 
-            # get nut base height
-            mask_rect = self.find_boundary_rect(hat_mask)
-            ty = max(0, mask_rect[0][1] - contour_expand_pixel)
-            by = min(item_img_h - 1, mask_rect[1][1] + contour_expand_pixel)
-            lx = max(0, mask_rect[0][0] - contour_expand_pixel)
-            rx = min(item_img_w - 1, mask_rect[1][0] + contour_expand_pixel)
-            base_img = item_img[ty:by, lx:rx]
-            # base_img = item_img[mask_rect[0][1] : mask_rect[1][1], mask_rect[0][0] : mask_rect[1][0]]
-            # get base
-            if base_img.size > 1:
-                base_mask = base_img > max_val - top_plane_offset
-                base_mask = (~base_mask) & (base_img != 0)
-                # base_height = np.median(base_img[base_mask])
-                base_height = np.percentile(base_img[base_mask], 45)
-            else:
-                base_mask = item_img > max_val - top_plane_offset
-                base_mask = (~base_mask) & (item_img != 0)
-                # base_height = np.median(item_img[base_mask])
-                base_height = np.percentile(item_img[base_mask], 45)
+            try:
+                # get nut base height
+                mask_rect = self.find_boundary_rect(hat_mask)
+                ty = max(0, mask_rect[0][1] - contour_expand_pixel)
+                by = min(item_img_h - 1, mask_rect[1][1] + contour_expand_pixel)
+                lx = max(0, mask_rect[0][0] - contour_expand_pixel)
+                rx = min(item_img_w - 1, mask_rect[1][0] + contour_expand_pixel)
+                base_img = item_img[ty:by, lx:rx]
+                # base_img = item_img[mask_rect[0][1] : mask_rect[1][1], mask_rect[0][0] : mask_rect[1][0]]
+                # get base
+                if at_least_r_depth is not None:
+                    top_plane_offset = at_least_r_depth
+                if base_img.size > 1:
+                    base_mask = base_img > max_val - top_plane_offset
+                    base_mask = (~base_mask) & (base_img > min_depth_value)
+                    base_height = np.median(base_img[base_mask])
+                    # base_height = np.percentile(base_img[base_mask], 45)
+                else:
+                    base_mask = item_img > max_val - top_plane_offset
+                    base_mask = (~base_mask) & (item_img > min_depth_value)
+                    base_height = np.median(item_img[base_mask])
+                    # base_height = np.percentile(item_img[base_mask], 45)
+            except:
+                base_height = 0
             # print(f"base_height {base_height}")
 
             # get relative depth
             relative_depth = np.abs(hat_height - base_height)
             # print(f"relative_depth {relative_depth}")
-            if relative_depth > depth_thres:
+
+            # detect
+            if relative_depth_thres < relative_depth < 100:
                 box.is_3ddefect = 1
                 box.is_defect = 1
+            elif relative_depth > 100:
+                # if it exceeds 100 something probably goes wrong, force it to be threshold
+                relative_depth = relative_depth_thres
 
             box.is_detected = 1
             box.value_3d = relative_depth
-            box.value_3dthres = depth_thres
-            box.description = f"Depth threshold: {box.value_3dthres}: Relative depth: {box.value_3d};"
+            box.value_3dthres = relative_depth_thres
+            box.description = f"{box.index}; Depth threshold: {box.value_3dthres}; Relative depth: {box.value_3d};"
             box.index = count
             new_item_bboxes_list.append(box)
             count += 1
+
+            if self.debug:
+                if self.logger:
+                    self.logger.info(box.description)
+                else:
+                    d_logger.info(box.description)
 
         return new_item_bboxes_list
 
