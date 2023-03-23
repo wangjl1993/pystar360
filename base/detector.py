7b6339df6551bbcf4f049731891f275fc3608b9c
mru4913 <yumengda@py-star.com>
2023-03-23 15:23:36 +0800

    feat(base): add pipeline runing algorithms

--- base/detector.py
+++ base/detector.py
@@ -1,4 +1,6 @@
 from importlib import import_module
+from typing import Dict, List
+from omegaconf import DictConfig, ListConfig
 
 from pystar360.base.dataStruct import bboxes_collector
 from pystar360.algo.algoBase import NullDetection
@@ -18,80 +20,85 @@ class Detector:
         # itemInfo
         self.device = device
         self.axis = axis
-        self.logger = logger
+        self.logger = logger if logger else d_logger
         self.debug = debug
         self.mac_password = mac_password
 
     def detect_items(self, item_bboxes, test_img, test_startline, img_w, img_h, **kwargs):
         if not item_bboxes:
             return []
-        item_bboxes_dict = bboxes_collector(item_bboxes)
 
-        if self.logger:
-            self.logger.info(f">>> Start detection...")
-        else:
-            d_logger.info(f">>> Start detection...")
+        # collect bboxes
+        item_bboxes_dict = bboxes_collector(item_bboxes)
+        self.logger.info(f">>> Start detection...")
 
         new_item_bboxes = []
         for label_name, item_bboxes_list in item_bboxes_dict.items():
-            if self.logger:
-                self.logger.info(f">>> Processing items with label: {label_name}")
-            else:
-                d_logger.info(f">>> Processing items with label: {label_name}")
+            self.logger.info(f">>> Processing items with label: {label_name}")
 
+            # load params
             try:
-                # load params
                 item_params = self.item_params[label_name]
             except KeyError:
-                if self.logger:
-                    self.logger.info(f">>> Params not provided. Skip items with label: {label_name}")
-                else:
-                    d_logger.info(f">>> Params not provided. Skip items with label: {label_name}")
+                self.logger.info(f">>> Params not provided. Skip items with label: {label_name}")
                 # new_item_bboxes.extend(item_bboxes_list)
                 continue
 
-            try:
-                # load func
-                # func_obj = eval(item_params.func)
-                func_obj = getattr(import_module(item_params.module), item_params.func)
-            except:
-                if self.logger:
-                    self.logger.info(f">>> Fail to call {item_params.func}")
-                else:
-                    d_logger.info(f">>> Fail to call {item_params.func}")
-                # new_item_bboxes.extend(item_bboxes_list)
-                continue
-
-            # init object
-            func = func_obj(
-                item_params=item_params.params,
-                device=self.device,
-                logger=self.logger,
-                axis=self.axis,
-                mac_password=self.mac_password,
-                debug=self.debug,
-            )
-            # sorting
-            if self.axis == 0:
-                item_bboxes_list = sorted(item_bboxes_list, key=lambda x: x.proposal_rect[0][1])
+            # check pipeline count
+            if isinstance(item_params, (Dict, DictConfig)):
+                item_params = [item_params]
+            elif isinstance(item_params, (List, ListConfig)):
+                pass
             else:
-                item_bboxes_list = sorted(item_bboxes_list, key=lambda x: x.proposal_rect[0][0])
+                raise ValueError(
+                    f">>> Item params {type(item_params)} for {label_name} is invalid. Please check your configure."
+                )
 
-            # run func
-            try:
-                local_item_bboxes = func(item_bboxes_list, test_img, test_startline, img_h, img_w, **kwargs)
-                new_item_bboxes.extend(local_item_bboxes)
-            except Exception as e:
-                if self.logger:
-                    self.logger.info(str(e))
+            # start looping
+            for idx, item_param in enumerate(item_params, start=1):
+                self.logger.info(f">>> Call method {idx} {item_param.func} for {label_name}.")
+
+                if self.debug:  # load func
+                    func_obj = getattr(import_module(item_param.module), item_param.func)
                 else:
-                    d_logger.info(str(e))
+                    try:
+                        func_obj = getattr(import_module(item_param.module), item_param.func)
+                    except:
+                        self.logger.info(f">>> Fail to call {item_param.func}")
+                        # new_item_bboxes.extend(item_bboxes_list)
+                        continue
 
-        if self.logger:
-            self.logger.info(f">>> Finished detection...")
-        else:
-            d_logger.info(f">>> Finished detection...")
+                # init object
+                func = func_obj(
+                    item_params=item_param.params,
+                    device=self.device,
+                    logger=self.logger,
+                    axis=self.axis,
+                    mac_password=self.mac_password,
+                    debug=self.debug,
+                )
 
+                # sorting
+                if self.axis == 0:
+                    item_bboxes_list = sorted(item_bboxes_list, key=lambda x: x.proposal_rect[0][1])
+                else:
+                    item_bboxes_list = sorted(item_bboxes_list, key=lambda x: x.proposal_rect[0][0])
+
+                # run algorithm
+                if self.debug:
+                    local_item_bboxes = func(item_bboxes_list, test_img, test_startline, img_h, img_w, **kwargs)
+                    item_bboxes_list = local_item_bboxes
+                else:
+                    try:
+                        local_item_bboxes = func(item_bboxes_list, test_img, test_startline, img_h, img_w, **kwargs)
+                        item_bboxes_list = local_item_bboxes
+                    except Exception as e:
+                        local_item_bboxes = item_bboxes_list
+                        self.logger.info(str(e))
+
+            new_item_bboxes.extend(local_item_bboxes)
+
+        self.logger.info(f">>> Finished detection...")
         return new_item_bboxes
 
     def _dev_item_null_detection_(self, item_bboxes, *args, **kwargs):
